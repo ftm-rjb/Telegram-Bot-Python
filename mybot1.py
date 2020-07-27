22972f2c110223eb0ed1ab627b5d0776e7a184d3
ftm-rjb <ftm.rjb7788@gmail.com>
2020-07-27 19:15:49 +0430

    updated

--- mybot1.py
+++ mybot1.py
@@ -1,13 +1,14 @@
 from telegram.ext import Updater , CommandHandler
 
-updater = Updater('1292823392:AAGV58CHQ9OxyR8YCCva7Ojmv1QqnZRHCNI')
+updater = Updater('1292823392:AAERdhM6yeCQaZPy4WdfkUBOn5vUyn7GEX4')
 
 def start(bot , update ):
     chat_id = update.message.chat_id
     bot.sendMessage(chat_id , 'your name is ')
 
 start_command = CommandHandler('start' , start )
-
-updater.dispatcher.add_handler(start_command)
-
+try:
+    updater.dispatcher.add_handler(start_command)
+except NetworkError():
+    pass
 updater.start_polling()
