dd07171091ae33387110908f3584fff3cf991f2f
ftm-rjb <ftm.rjb7788@gmail.com>
2020-07-27 17:24:36 +0430

    updated

--- mybot1.py
+++ mybot1.py
@@ -2,14 +2,12 @@ from telegram.ext import Updater , CommandHandler
 
 updater = Updater('1292823392:AAGV58CHQ9OxyR8YCCva7Ojmv1QqnZRHCNI')
 
-def start(bot , update , args):
-    bot.sendChatAction(update.message.chat_id , 'TYPING');
+def start(bot , update ):
+    chat_id = update.message.chat_id
+    bot.sendMessage(chat_id , 'your name is ')
 
-    bot.sendMessage(update.message.chat_id , 'your name is :' + args[0])
-
-start_command = CommandHandler('start' , start , pass_args = True)
+start_command = CommandHandler('start' , start )
 
 updater.dispatcher.add_handler(start_command)
 
 updater.start_polling()
-updater.idle()
