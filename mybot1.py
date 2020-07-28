e91ca4332cd23ed7d386f902b8aca17a99ceab48
ftm-rjb <ftm.rjb7788@gmail.com>
2020-07-28 16:14:14 +0430

    1

--- mybot1.py
+++ mybot1.py
@@ -1,15 +1,14 @@
 from telegram.ext import Updater , CommandHandler
+TOKEN = '1292823392:AAERdhM6yeCQaZPy4WdfkUBOn5vUyn7GEX4'
+REQUEST_KWARGS={'proxy_url': 'http://188.120.232.181:8118/'}
 
-updater = Updater('1292823392:AAERdhM6yeCQaZPy4WdfkUBOn5vUyn7GEX4')
+updater = Updater(TOKEN, request_kwargs=REQUEST_KWARGS)
 
 def start(bot , update ):
     chat_id = update.message.chat_id
     bot.sendMessage(chat_id , 'your name is ')
 
 start_command = CommandHandler('start' , start )
-try:
-    updater.dispatcher.add_handler(start_command)
-except NetworkError():
-    pass
+updater.dispatcher.add_handler(start_command)
 updater.start_polling()
 print('working')
