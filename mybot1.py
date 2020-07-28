3e1bef34328f4375518c26f771fc71d63b40c031
ftm-rjb <ftm.rjb7788@gmail.com>
2020-07-28 17:22:25 +0430

    1

--- mybot1.py
+++ mybot1.py
@@ -1,13 +1,18 @@
+import telegram
 from telegram.ext import Updater , CommandHandler
-TOKEN = '1292823392:AAHyY7c9Scd3dw429rz6rPct1ohy5_cERRg'
 
-updater = Updater(TOKEN)
+tok = '1292823392:AAHyY7c9Scd3dw429rz6rPct1ohy5_cERRg'
 
 def start(bot , update ):
     chat_id = update.message.chat_id
-    bot.sendMessage(chat_id , 'your name is ')
+    bot.sendMessage(chat_id , 'hellow')
+
+mybot = telegram.Bot(token=tok)
+dictinfo=mybot.get_me()
+
+myupdater = Updater(token=tok, use_context=True)
+mydispatcher = myupdater.dispatcher
 
 start_command = CommandHandler('start' , start )
-updater.dispatcher.add_handler(start_command)
+mydispatcher.add_handler(start_command)
 updater.start_polling()
-print('working')
