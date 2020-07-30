79fcfe2c5ce0aff127de41a41c9575a045161b74
unknown <ftm.rjb7788@gmail.com>
2020-07-30 16:05:19 +0430

    '1'

--- mybot1.py
+++ mybot1.py
@@ -1,12 +1,13 @@
 from telegram.ext import Updater , CommandHandler
 
-updater = Updater('1329017306:AAG6tozyUJqhZsUT9X-s-JE9JPCqQDnw4Wo')
+updater = Updater(token = '1225980698:AAHqrM404mnvFhtezguH7oUW9xqAL2Egs_E' , 
+                  request_kwargs = {'proxy_url': 'socks5://127.0.0.1:9050/'})
 
-def hi(bot , update):
+def register(bot , update):
     chat_id = update.message.chat_id
     bot.sendChatAction(chat_id , 'TYPING')
     bot.sendMessage(chat_id , 'hello')
 
-start_command = CommandHandler('hi' , hi)
+start_command = CommandHandler('register' , register)
 updater.dispatcher.add_handler(start_command)
 updater.start_polling()
