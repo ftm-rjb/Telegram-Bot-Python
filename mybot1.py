741e34bd7e14512e3d8c88642cc01cbb36ec6251
unknown <ftm.rjb7788@gmail.com>
2020-07-31 15:25:03 +0430

    first commit

--- mybot1.py
+++ mybot1.py
@@ -1,12 +1,21 @@
 from telegram.ext import Updater , CommandHandler
-
-updater = Updater(token = '1329017306:AAG6tozyUJqhZsUT9X-s-JE9JPCqQDnw4Wo')
-
+import os
+PORT = int(os.environ.get('PORT', 5000))
+TOKEN = '1329017306:AAG6tozyUJqhZsUT9X-s-JE9JPCqQDnw4Wo'
 def register(bot , update):
     chat_id = update.message.chat_id
     bot.sendChatAction(chat_id , 'TYPING')
     bot.sendMessage(chat_id , 'hello')
+    
+def main():    
+    updater = Updater(TOKEN , use_context=True)
+    start_command = CommandHandler('hi' , register)
+    updater.dispatcher.add_handler(start_command)
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.setWebhook('https://desolate-bayou-80201.herokuapp.com/' + TOKEN)
+    updater.idle()
 
-start_command = CommandHandler('hi' , register)
-updater.dispatcher.add_handler(start_command)
-updater.start_polling()
+if __name__ == '__main__':
+    main()
