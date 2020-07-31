344a189c4cc49d8920413d9d8cdc523506ca3ce0
unknown <ftm.rjb7788@gmail.com>
2020-07-31 12:49:42 +0430

    '1'

--- mybot1.py
+++ mybot1.py
@@ -1,12 +1,12 @@
 from telegram.ext import Updater , CommandHandler
 
-updater = Updater('1225980698:AAHqrM404mnvFhtezguH7oUW9xqAL2Egs_E')
+updater = Updater(token = '1329017306:AAG6tozyUJqhZsUT9X-s-JE9JPCqQDnw4Wo')
 
 def register(bot , update):
     chat_id = update.message.chat_id
     bot.sendChatAction(chat_id , 'TYPING')
     bot.sendMessage(chat_id , 'hello')
 
-start_command = CommandHandler('register' , register)
+start_command = CommandHandler('hi' , register)
 updater.dispatcher.add_handler(start_command)
 updater.start_polling()
