c095798c4bc9574b1626ba895bd28096faf063e6
ftm-rjb <ftm.rjb7788@gmail.com>
2020-08-01 20:05:54 +0430

    1

--- mybot1.py
+++ mybot1.py
@@ -5,19 +5,25 @@ TOKEN = '1329017306:AAEdUNxL56_7y9orx7ci8ak5-pl4C-GbDmA'
 #REQUEST_KWARGS={'proxy_url': 'https://2.188.17.71:8080/'}
 
 
-def hi(bot , update , args):
+def start(bot , update , args):
     chat_id = update.message.chat_id
+    first = update.message.chat.first_name
+    last = update.message.chat.last_name
     bot.sendChatAction(chat_id , 'TYPING')
-    bot.sendMessage(chat_id , 'hello' + args[0])
-    bot.sendMessage(chat_id , 'your id is:' + args[1])
-    bot.send_contacts(chat_id)
+    bot.sendMessage(chat_id , 'hello {first} {last}'.format(first = first , last = last))
+
+def contacts(bot , update ):
+    chat_id = update.message.chat_id
+    bot.sendContacts(chat_id , phone_number=09125384132, first_name='mom', last_name='mah')
 
 def main():
     updater = Updater(TOKEN)#request_kwargs=REQUEST_KWARGS)
-    hi_command = CommandHandler('hi' , hi , pass_args = True)
+    start_command = CommandHandler('start' , start , pass_args = True)
+    contacts_command = CommandHandler('contacts' , contacts)
     #one_massage = MessageHandler(Filters.all , hi)
     #one_poll = PollAnswerHandler(hi)
-    updater.dispatcher.add_handler(hi_command)
+    updater.dispatcher.add_handler(start_command)
+    updater.dispatcher.add_handler(contacts_command)
     #updater.dispatcher.add_handler(one_massage)
     #updater.dispatcher.add_handler(one_poll)
     updater.start_polling()
