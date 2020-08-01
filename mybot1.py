ca6b8a5c22888b55c3642257acdd4936e1bab004
ftm-rjb <ftm.rjb7788@gmail.com>
2020-08-01 16:59:19 +0430

    1

--- mybot1.py
+++ mybot1.py
@@ -1,24 +1,31 @@
-from telegram.ext import Updater , CommandHandler
-import os
-PORT = int(os.environ.get('PORT', 5000))
+from telegram.ext import Updater , CommandHandler , MessageHandler , filters ,PollAnswerHandler
+#import os
+#PORT = int(os.environ.get('PORT', 5000))
 TOKEN = '1329017306:AAEdUNxL56_7y9orx7ci8ak5-pl4C-GbDmA'
 #REQUEST_KWARGS={'proxy_url': 'https://2.188.17.71:8080/'}
 
 
-def register(bot , update):
+def hi(bot , update , args):
     chat_id = update.message.chat_id
     bot.sendChatAction(chat_id , 'TYPING')
-    bot.sendMessage(chat_id , 'hello')
-    
-def main():    
+    bot.sendMessage(chat_id , 'hello' + args[0])
+    bot.sendMessage(chat_id , 'your id is:' + args[1])
+
+def main():
     updater = Updater(TOKEN)#request_kwargs=REQUEST_KWARGS)
-    start_command = CommandHandler('hi' , register)
-    updater.dispatcher.add_handler(start_command)
+    hi_command = CommandHandler('hi' , hi)
+    one_massage = MessageHandler(Filters.all , hi)
+    one_poll = PollAnswerHandler(hi)
+    updater.dispatcher.add_handler(hi_command)
+    updater.dispatcher.add_handler(one_massage)
+    updater.dispatcher.add_handler(one_poll)
     updater.start_polling()
     updater.idle()
 
+
 if __name__ == '__main__':
     main()
+    updater.stop()
 '''updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN)
